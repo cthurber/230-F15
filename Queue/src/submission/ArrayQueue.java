a3459b7ffd82dd58a5533cce25d8b06430d19874
Chris Thurber <cthurber@me.com>
2015-11-06 10:30:38 -0500

    Implemented AQ

---
+++ Queue/src/submission/ArrayQueue.java
@@ -0,0 +1,67 @@
+package submission;
+
+import adt.Queue;
+
+public class ArrayQueue<T> implements Queue<T> {
+	private T[] data;
+	private int head, tail;
+	private static int DEFAULT_CAPACITY = 100;
+	
+    public ArrayQueue() {
+       tail = 0;
+       data = (T[])(new Object[DEFAULT_CAPACITY]);
+    }
+	
+    public ArrayQueue (int initialCapacity) {
+       tail = 0;
+       data = (T[])(new Object[initialCapacity]);
+    }
+    
+	@Override
+	public void enqueue (T element) {
+       data[tail] = element;
+       tail++;
+    }
+
+	@Override
+    public T dequeue() {
+       if (isEmpty())
+          return null;
+ 
+       T result = data[0];
+       tail--;
+       for (int look=0; look < tail; look++)
+          data[look] = data[look+1];
+       return result;
+    }
+
+	@Override
+	public T peek() {
+       if (isEmpty())
+          return null;  
+       return data[0];
+    }
+
+	@Override
+	public boolean isEmpty() {
+		return tail==0;
+	}
+
+	@Override
+	public void clear() {
+		// Less efficient: while(!isEmpty()) this.dequeue();
+		data = (T[])(new Object[DEFAULT_CAPACITY]);
+		tail = 0;
+	}
+	
+	public String toString() {
+		String s = "";
+		if (!isEmpty())
+			s = data[head].toString();
+		for (int i = (head+1) % data.length; i <= (tail + data.length-1) % data.length; i++)
+			s += " -> " + data[i];
+		s+= "\n";
+		return s;
+	}
+
+}
