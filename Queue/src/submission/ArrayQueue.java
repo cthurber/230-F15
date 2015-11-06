14272948c36667115c2df1c96038ffd790ca5fa9
Chris Thurber <cthurber@me.com>
2015-11-05 23:09:53 -0500

    Working on ArrayQueue Implementation

--- Queue/src/submission/ArrayQueue.java
+++ Queue/src/submission/ArrayQueue.java
@@ -5,46 +5,64 @@ import adt.Queue;
 public class ArrayQueue<T> implements Queue<T> {
 	private T[] data;
 	private int head, tail = 0;
-	private int size = data.length;
+	private static int DEFAULT_CAPACITY = 100;
+	
+	/*
+    public ArrayQueue() {
+       tail = 0;
+       data = (T[])(new Object[DEFAULT_CAPACITY]);
+    }
+	
+    public ArrayQueue (int initialCapacity) {
+       tail = 0;
+       data = (T[])(new Object[initialCapacity]);
+    }
+    */
 	
 	@Override
-	public void enqueue(T newEntry) {
-		if(size==0) {
-			data[head] = newEntry;
-		} else {
-			int index = 0;
-			while(data[index]!=null){
-				index++;
-			}
-			tail = index;
-			data[index] = newEntry;
-		}
-		
-	}
+	public void enqueue (T element) {
+//       if (size() == data.length) 
+//          expandCapacity();
+       data[tail] = element;
+       tail++;
+    }
 
 	@Override
-	public T dequeue() {
-		T current = data[tail];
-		tail--;
-		return current;
-	}
+    public T dequeue() {
+       if (isEmpty())
+          return null;
+ 
+       T result = data[0];
+ 
+       tail--;
+ 
+       /** shift the elements  */
+       for (int scan=0; scan < tail; scan++)
+          data[scan] = data[scan+1];
+       
+       data[tail] = null;
+ 
+       return result;
+    }
 
 	@Override
 	public T peek() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+       if (isEmpty())
+          return null;  
+       return data[0];
+    }
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return tail==0;
 	}
 
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		
+		data = (T[]) new Object[DEFAULT_CAPACITY];
+		head = 0;
+		tail = 0;
 	}
 	
 	public String toString() {
